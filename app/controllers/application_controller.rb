4b76d21bc9997952b754da42f5f3e2245227daca
Justin Capalbo <capalbo.justin@gmail.com>
2017-05-23 00:07:17 +0000

    Under construction

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def hello
-    render html: "Hello, World!  ¡Hola, Mundo!"
+    render 'layouts/application'
   end
   
   def goodbye
