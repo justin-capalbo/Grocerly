db35ef3f65b5b68acbb4ab5d12820e9838caac63
Justin Capalbo <capalbo.justin@gmail.com>
2017-05-24 01:34:22 +0000

    Setup Devise and basic site layout
    commit 1fad2cd7323e0f6a960334adb3fbc25572e166ad
    Author: Justin Capalbo <capalbo.justin@gmail.com>
    Date:   Tue May 23 04:31:51 2017 +0000
    
        Setup username as the authentication key
    
        The initializer devise.rb contains a key field called
        config.authentication_keys which has to be initialized to the new field
        username which we add to users with a migration in this step.  The
        Devise fiews for the registrations and sessions controllers have been
        revised to make use of the username rather than the email as the main
        key field for logging in, both by adding it and requiring it on login.
    
        application_controller.rb also has an important job which is to permit
        certain keys for various actions in the Devise controllers, namely
        sign_in, sign_up, and account_update.
    
        Also tweak layout CSS by making everything normal containers.  After
        this, we are ready to merge into dev.
    
    commit fbb91456b216e276223528bb9211420a79386d2f
    Author: Justin Capalbo <capalbo.justin@gmail.com>
    Date:   Tue May 23 01:42:48 2017 +0000
    
        Revise routes and set up before_action
    
        before_action for the pages controller is used to run the
        authenticate_user! method which prevents the controller's pages (for
        certain actions) from being viewable without authentication by
        redirecting the user to the Devise view instead.  Other changes include
        actually designing view for pages#index that the user will see after
        logged in, which at this point is the revised construction partial.
    
    commit d2093d66a16d6086aff3dff27257e292227a36d6
    Author: Justin Capalbo <capalbo.justin@gmail.com>
    Date:   Tue May 23 01:30:28 2017 +0000
    
        Add Pages Controller, Devise views, User Model
    
        Generating the Devise views creates a number of views for the various
        controllers that Devise bundles.  Creating the user model via the
        generator bundled with Devise creates a user that has several important
        preconfigurations.
    
    commit c53f8aff00142c47fec611391bd6c72dcd15fab5
    Author: Justin Capalbo <capalbo.justin@gmail.com>
    Date:   Tue May 23 00:44:41 2017 +0000
    
        Add Devise gems and slightly tweak layout
    
        The beginning of the layout requires having an alert section for display
        of flash messages that will be generated by Devise.  Installing Devise
        also generates few more files.
    
        Cleaned up construction and layout by moving the construction specific stuff
        into its own .erb file instead of having it sit in the main layout,
        which needs to have breathing room for the nav bar and alert partial.
        It's possible that it will be added back later but the main purpose of
        this branch is to see how Devise looks without other things interfering.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  root 'application#hello'
   
-  get '/hello'   => 'application#hello'
-  get '/goodbye' => 'application#goodbye'
+  devise_for :users
+  root 'pages#index'
+  
 end
