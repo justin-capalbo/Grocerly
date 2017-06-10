226e42685f35b85046992e6be1ce3a8d37becc1d
Justin Capalbo <capalbo.justin@gmail.com>
2017-06-10 16:09:10 -0400

    Change from sqlite3 to postgresql
    This is a change to improve consistency between environments.  The
    switch was very easy and now the sqlite3 databases are gone.  Closes #9

--- db/schema.rb
+++ db/schema.rb
@@ -12,10 +12,13 @@
 
 ActiveRecord::Schema.define(version: 20170604015822) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "items", force: :cascade do |t|
     t.string "name"
     t.text "notes"
-    t.integer "list_id"
+    t.bigint "list_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["created_at", "id"], name: "index_items_on_created_at_and_id"
@@ -24,7 +27,7 @@ ActiveRecord::Schema.define(version: 20170604015822) do
 
   create_table "lists", force: :cascade do |t|
     t.string "name"
-    t.integer "user_id"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["created_at", "id"], name: "index_lists_on_created_at_and_id"
@@ -49,4 +52,6 @@ ActiveRecord::Schema.define(version: 20170604015822) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "items", "lists"
+  add_foreign_key "lists", "users"
 end
