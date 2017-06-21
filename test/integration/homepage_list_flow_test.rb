d31743d80d232a7c44dce94680966a5c87b27633
Justin Capalbo <capalbo.justin@gmail.com>
2017-06-20 22:29:19 -0400

    Test for presence of delete mode button

--- test/integration/homepage_list_flow_test.rb
+++ test/integration/homepage_list_flow_test.rb
@@ -4,21 +4,28 @@ class HomepageListFlowTest < ActionDispatch::IntegrationTest
   include Devise::Test::IntegrationHelpers
   def setup
     @user = create(:user_with_items)
-    @barb = create(:user_with_no_items)
+    @user_no_items = create(:user_with_no_items)
   end
   
   test "item creation" do
-    sign_in @barb
+    sign_in @user_no_items
     get root_path
 
+    # there shouldn't be a delete button when there are no items
+    assert_select 'div.btn-delete-mode', count: 0
+    assert_select 'div.btn-delete', count: 0
+
     # I can add an item to my own list
-    @list = @barb.lists.first
+    @list = @user_no_items.lists.first
     assert_difference 'Item.count', 1 do
       post items_path, params: { list_id: @list.id,
                                  item: { name: "New Item",
                                          notes: "Notes" } }
     end
     assert_not flash.empty?
+    assert_redirected_to root_url
+    follow_redirect!
+    assert_select 'div.btn-delete-mode', count: 1
 
     # I can't add an item to someone elses list
     other_list = @user.lists.first
@@ -52,7 +59,7 @@ class HomepageListFlowTest < ActionDispatch::IntegrationTest
     end
 
     # Can't delete another user's item
-    sign_in @barb 
+    sign_in @user_no_items 
     get root_path
     assert_select "a[href=?]", item_path(@second_item), count: 0
     assert_no_difference 'Item.count' do
